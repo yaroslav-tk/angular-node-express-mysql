d5960f404169d4e9bfac55042ec50e41f38d20f6
Yaroslav Tkachenko <yaroslav.tka4enko@gmail.com>
2023-09-10 17:06:00 +0300

    deploy

--- angular-node-express-mysql-frontend/src/app/core/user/services/user.service.ts
+++ angular-node-express-mysql-frontend/src/app/core/user/services/user.service.ts
@@ -2,23 +2,25 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/models/user.model';
+import { environment } from '../../environments/environment.prod';
 
 @Injectable()
 export class UserService {
+  private baseURL = environment.baseUrl;
 
   constructor(
     private http: HttpClient
   ) { }
 
   getUser(): Observable<User> {
-    return this.http.get<User>('https://angular-node-express-mysql-backend.onrender.com/api/user/me')
+    return this.http.get<User>(`${this.baseURL}/api/user/me`)
   }
 
   getCompanyUsers(): Observable<User[]> {
-    return this.http.get<User[]>('/api/user/users')
+    return this.http.get<User[]>(`${this.baseURL}/api/user/users`)
   }
 
   editUser(user: User): Observable<User> {
-    return this.http.put<User>('/api/user/edit', user)
+    return this.http.put<User>(`${this.baseURL}/api/user/edit`, user)
   }
 }
